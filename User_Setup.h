da1e8942c6a9741cedf58229f120709b102d0153
ibtdev26 <ryan_6j@hotmail.com>
2026-01-17 17:20:28 +0000

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -1,3 +1,5 @@
+#define TFT_SPI_MODE SPI_MODE3
+
 #define ILI9341_DRIVER
 
 #define TFT_MOSI 5
@@ -13,7 +15,6 @@
 #define SPI_TOUCH_FREQUENCY 2500000
 
 // REQUIRED fixes
-#define TFT_SPI_MODE SPI_MODE0
 #define TFT_INIT_DELAY 120
 
 // Disable DMA (good call)
