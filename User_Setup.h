0bf6eba564e57abc5d257242dd31dda2f24e9ae3
ibtdev26 <ryan_6j@hotmail.com>
2026-01-17 17:13:29 +0000

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -1,5 +1,3 @@
-
-
 #define ILI9341_DRIVER
 
 #define TFT_MOSI 5
@@ -9,11 +7,14 @@
 #define TFT_DC   2
 #define TFT_RST  3
 
-#define TFT_WIDTH  240
-#define TFT_HEIGHT 320
-
-#define SPI_FREQUENCY  20000000   // 20MHz (SAFE)
+// SPI settings – SAFE for ESP32-C5 + ILI9341
+#define SPI_FREQUENCY        20000000
 #define SPI_READ_FREQUENCY  10000000
-#define SPI_TOUCH_FREQUENCY  2500000
+#define SPI_TOUCH_FREQUENCY 2500000
 
+// REQUIRED fixes
+#define TFT_SPI_MODE SPI_MODE0
+#define TFT_INIT_DELAY 120
+
+// Disable DMA (good call)
 #define TFT_NO_DMA
